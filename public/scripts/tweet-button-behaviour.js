1d566e74c7c7b5a1f85a0deb6ac702d4f52d8c55
double-slide <93881358+double-slide@users.noreply.github.com>
2022-04-01 01:16:32 +0000

    Code clean-up & debug for duplicate tweets

--- public/scripts/tweet-button-behaviour.js
+++ public/scripts/tweet-button-behaviour.js
@@ -16,39 +16,5 @@ $(document).ready(function() {
       "border": "0px solid #4056A1",
       "margin": "8px"
     });
-  });
-
-  
-$(".retweetButtons > .fa-solid").on("mouseover", function(event) {
-  $(this).css({color: "red"});  
-});
-
-$(".retweetButtons > .fa-solid").on("mouseleave", function(event) {
-  $(this).css({color: "grey"});
-});
-
-$("article").on("mouseover", function(event) {
-  $(this).css({
-    "box-shadow": "5px 5px",
-    "background-color": "#f6f5ff"
-  });
-});
-
-$("article").on("mouseleave", function(event) {
-  $(this).css({
-    "box-shadow": "none",
-    "background-color": "white"
-  });
-});
-
-
-
-
-
-
-
-
-
-
-  
+  });  
 });
